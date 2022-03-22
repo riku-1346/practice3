e49114aaec3e1bbf462d2c6ac937ca53d70c4e98
riku-1346 <93707011+riku-1346@users.noreply.github.com>
2022-03-23 02:28:36 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+# practice3$(function(){
+  /*=================================================
+  スマホメニュー
+  ===================================================*/
+  // ハンバーガーメニューのクリックイベント
+  // 解説は、「中級編：ストアサイト（インテリア）」参照
+  $('.toggle_btn').on('click', function() {
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
+    } else {
+      $('#header').addClass('open');
+    }
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $('#mask').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $('#navi a').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内リンクのイベント
+  $('a[href^="#"]').click(function(){
+    // リンクを取得
+    const href= $(this).attr("href");
+    // ジャンプ先のid名をセット
+    const target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    const position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+
+  /*=================================================
+  PICK UP スライダー
+  ===================================================*/
+  // カルーセル用 jQueryプラグイン「slick」の設定
+  // マニュアル：https://kenwheeler.github.io/slick/
+  $('.slick-area').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  /*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function() {
+    // fadeinクラスに対して順に処理を行う
+    $('.fadein').each(function() {
+      // スクロールした距離
+      const scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      const target = $(this).offset().top;
+      // 画面の高さ
+      const windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
+});
